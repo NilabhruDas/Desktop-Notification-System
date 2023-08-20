2a599103cc1cd40dc897b94ba88680c75d9a40f0
Nilabhru Das <105483272+NilabhruDas@users.noreply.github.com>
2023-08-20 20:55:47 +0530

    Update Desktop_Notification_System.py

--- Desktop_Notification_System.py
+++ Desktop_Notification_System.py
@@ -1,11 +1,34 @@
 from plyer import notification
 import time
+import pyttsx3
+
+while True:
+    try:
+        interval = int(input("Enter the desired time interval (in seconds): "))
+        if interval <= 0:
+            print("Interval must be a positive integer.")
+        else:
+            break
+    except ValueError:
+        print("Please enter a valid integer.")
+
+engine = pyttsx3.init()
+msge = input("Enter the message: ")
 
 if __name__ == '__main__':
-        while True:
+    while True:
+        try:
             notification.notify(
-                title = "Take some rest buddy",
-                message = "It is very important of your health",
-                app_icon = "D:/vs code files/Python/Desktop Notification System/icon.ico",
-            timeout = 5 )
-            time.sleep(3600)
+                title="Take some rest buddy",
+                message=msge,
+                app_icon="D:/vs code files/Python/Desktop Notification System/icon.ico",
+                timeout=5
+            )
+            engine.say(msge)
+            engine.runAndWait()
+            time.sleep(interval)
+        except KeyboardInterrupt:
+            print("Notification loop stopped by user.")
+            break
+        except Exception as e:
+            print(f"An error occurred: {e}")
